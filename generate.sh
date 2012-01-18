e7f099f9018ce1d96b3fc3dd5721b476be58df8e
Riku <lu.riku@gmail.com>
2012-01-18 11:19:36 +0800

    update

--- generate.sh
+++
@@ -1,8 +0,0 @@
-#generate index.html
-perl Markdown_1.0.1/Markdown.pl syntax.md > content.html
-cat header.html content.html footer.html > index.html
-
-#generate basic.html
-perl Markdown_1.0.1/Markdown.pl basics.md > basic_content.html
-cat header.html basic_content.html footer.html >> basic.html
-
